f3e3f3b88add10be3f616759ed6b97a0db54f97f
Gufran Khurshid <gufran.khurshid@fastfox.com>
2017-03-16 16:19:13 +0530

    Third Commit

--- app/src/main/java/com/gufran/retrofitstudyapp/rest/ApiClient.java
+++ app/src/main/java/com/gufran/retrofitstudyapp/rest/ApiClient.java
@@ -1,5 +1,6 @@
 package com.gufran.retrofitstudyapp.rest;
 
+import okhttp3.Credentials;
 import retrofit2.Retrofit;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
@@ -15,14 +16,34 @@ public class ApiClient {
     public static final String BASE_URL = "http://1.worldrapradio.appspot.com/";
     private static Retrofit retrofit = null;
 
+
+    String credentials = Credentials.basic("aUsername", "aPassword");
+
     public static Retrofit getClient() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    //.client(okHttpClient)
                     // .addConverterFactory(GsonConverterFactory.create())
                     .addConverterFactory(ScalarsConverterFactory.create())
                     .build();
         }
+
+
+//        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+//                .addInterceptor(new Interceptor() {
+//                    @Override
+//                    public okhttp3.Response intercept(Chain chain) throws IOException {
+//                        Request originalRequest = chain.request();
+//                        Request.Builder builder = originalRequest.newBuilder().header("Authorization",
+//                                credentials);
+//                        Request newRequest = builder.build();
+//                        return chain.proceed(newRequest);
+//                    }
+//                }).build();
+
+
+
         return retrofit;
     }
 }
--- app/src/main/java/com/gufran/retrofitstudyapp/rest/ApiInterface.java
+++ app/src/main/java/com/gufran/retrofitstudyapp/rest/ApiInterface.java
@@ -2,6 +2,8 @@ package com.gufran.retrofitstudyapp.rest;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Description ...
@@ -18,6 +20,26 @@ public interface ApiInterface {
     @GET("UserPrefrenceAction?radiostationid=http://108.61.73.117:14022&userid=IORT5637&instruction=voteup")
     Call<String> getRadioStationRaw();
 
+    ////SAMPLES
+//    @GET("users/{name}/commits")
+//    Call<List<Commit>> getCommitsByName(@Path("name") String name)
+
+
+//    @GET("users")
+//    Call<User> getUserById(@Query("id") Integer id)
+
+
+//    @POST("users")
+//    Call<User> postUser(@Body User user)
+
+
+//    @GET("user")
+//    Call<UserDetails> getUserDetails(@Header("Authorization") String credentials
+
+
 //    @GET("movie/{id}")
 //    Call<MoviesResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
+
+    //@GET("movie/{id}")
+    // Call<MoviesResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
 }
